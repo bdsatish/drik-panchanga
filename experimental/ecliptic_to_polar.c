85dc67e413034a27df8c6fb448f2992b70d0a9ef
Satish BD <bdsatish@gmail.com>
2023-02-07 08:23:51 +0000

    More tables of comparison to BSS, SS, GL and Sewell/Dixit
    BSS = brahma sphuta siddhanda
    SS = surya siddhanta
    GL = graha laghava

--- experimental/ecliptic_to_polar.c
+++ experimental/ecliptic_to_polar.c
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    double gh_day;
+    double gh_day, bh_day, ps_day;
     char star[40];
     double x_equ[6] = {0};
     double x_sid[6] = {0};
@@ -90,31 +90,45 @@ int main(int argc, char* argv[])
     pl = equatorial_to_ss_polar(x_equ[0], epsln); // equatorial to SS polar long
     printf("Jyestha at SS polar longitude %.7f on its zero-day ayan\n", pl);
 
+    // Varahamihira's pancha-siddhantika day
+    ps_day = swe_julday(505, 6, 21, 6.0, SE_JUL_CAL); // approx date. some say 575 A.D.
+    // brahma sphuta siddhanta (BSS) positions
+    bh_day = swe_julday(628, 6, 21, 6.0, SE_JUL_CAL); // some date in 628 A.D.
     // Graha-laghava positions
     gh_day = swe_julday(1520, 3, 19, 6.0, SE_JUL_CAL);
     epsln = obliquity(gh_day);
-    double expected = 48.5;
-    xx_sid[0] = 47; xx_sid[1] = -5.6; xx_sid[2] = 1.0;
+    double expected = 49.5;
+    xx_sid[0] = 48.83; xx_sid[1] = -5.61; xx_sid[2] = 1.0;
     swe_cotrans(xx_sid, x_equ, -epsln); // ecliptic to equatorial coords
     pl = equatorial_to_ss_polar(x_equ[0], epsln); // equatorial to SS polar long
-    printf("Rohini (Aldebaran) sidereal [%.4f, %.4f] at SS polar longitude %.7f on its grha-laghava day. Expected %.1f\n",
+    printf("Anuradha (Dscubba) sidereal [%.4f, %.4f] at SS polar longitude %.7f on its graha-laghava day. Expected %.1f (GL)\n",
            xx_sid[0], xx_sid[1], pl, expected);
 
     swe_close();
 }
 
-// In summary: Graha-laghava gives many matches to its table, if we assume TRUE_REVATI. Pancha-siddhantika gives better matches to TRUE_CITRA.
+// In summary:
+// Graha-laghava, SS and BSS match many coordinates if we assume TRUE_REVATI.
+// Only Pancha-siddhantika gives matches close to TRUE_CITRA.
+// S.S. says that Yogatara of Rohini, Punarvasu, Ashlesha and Mula lies on the "eastern" (not middle, i.e., middle point + some 1-4 degrees)
+// S.S. says that Yogatara of Dhanistha is "western".
+// S.S. says that Yogatara of Jyestha, Sravana, Anuradha, Pushya lies in the "middle".
+// S.S. says that Yogatara of P.Phal, U.Phal, P.Bhad, U.Bhad, P.Asadha, U.Ashadha, Asvini, Visakha, Mrigasira lies in the "northern"
 //
 // Rohini-paksha ayanamsha seems best compromise. Satisfies all three -- SS, GL and PS.
-// Rohini (Aldebaran) sidereal [46.6666, -5.6000] at SS polar longitude 48.2923257 on its graha-laghava day. Expected 49.00 (GL), 48.0 (Panchasiddhantika), 49.50 (Surya Siddhanta) --> zero ayan day at [+0340/07/10 21.4746]
-// Rohini (Aldebaran) sidereal [47.0000, -5.6000] at SS polar longitude 48.6154108 on its grha-laghava day.  Expected 49.00 (GL), 48.0 (Panchasiddhantika), 49.50 (Surya Siddhanta) --> Zero ayan day at [+0364/07/11 23.2122]
-// Punarvasu (Pollux) sidereal [93.3333, 6.5300] at SS polar longitude 93.507844 on its grha-laghava day. Expected 94.0 (GL), 88 (Panchasiddhantika), 93 (SS)
-// Magha (Regulus) sidereal [126.6666, 0.3790] at SS polar longitude 126.7652541 on its graha-laghava day. Expected 129.00 (GL), 126.0 (Panchasiddhantika), 129 (SS)
-// Citra (Spica) sidereal [180.0000, -1.9220] at SS polar longitude 179.1638906 on its graha-laghava day. Expected 183.00 (GL), 180°50' (Panchasiddhantika), 180 (SS)
-// Jyestha (Antares) sidereal [226.6666, -4.3512] at SS polar longitude 225.3336877 on its graha-laghava day. Expected 230.00 (GL), 229 (SS)
-// Mula (Sabik) sidereal [234.1200, 7.2000] at SS polar longitude 235.8856992 on its graha-laghava day. Expected 242.0 (GL), 241 (SS)
-// Mula (Shaula) sidereal [241.0000, -13.5100] at SS polar longitude 237.8089711 on its grha-laghava day. Expected 242.0 (GL), 241 (SS)
-// Anuradha (Dscubba) sidereal [220.0000, -1.7300] at SS polar longitude 219.4185972 on its grha-laghava day. Expected 224.0 (GL), 224 (SS)
-// Ashlesha (epHya) sidereal [109.0000, -11.2400] at SS polar longitude 107.5099665 on its grha-laghava day. Expected 107.0 (GL), 107.666 (Panchasiddhantika), 109 (SS)
-// Pushya (deCnc) sidereal [106.0000, -0.0030] at SS polar longitude 105.9996404 on its grha-laghava day. Expected 106.0 (GL), 97.333 (Panchasiddhantika), 106 (SS)
-// Revati (zePsc) sidereal [359.6660, -0.2400] at SS polar longitude -0.2296270 on its grha-laghava day. Expected 0.0, 359°50' (SS)
+// Rohini (Aldebaran) sidereal [46.6666, -5.6000] at SS polar longitude 48.2923257 on its graha-laghava day. Expected 49.00 (GL), 48.0 (Panchasiddhantika), 49°30 (Surya Siddhanta), 49°28' (BSS) --> zero ayan day at [+0340/07/10 21.4746]
+// Rohini (Aldebaran) sidereal [47.0000, -5.6000] at SS polar longitude 48.6154108 on its graha-laghava day. Expected 49.00 (GL), 48.0 (Panchasiddhantika), 49°30 (Surya Siddhanta), 49°28' (BSS) --> Zero ayan day at [+0364/07/11 23.2122]
+// Rohini (Aldebaran) sidereal [48.3333, -5.6000] at SS polar longitude 49.9069991 on its graha-laghava day. Expected 49.00 (GL), 48.0 (Panchasiddhantika), 49°30 (Surya Siddhanta), 49°28' (BSS) --> Zero ayan day at [+0460/07/13 10.8118]
+// Punarvasu (Pollux) sidereal [93.3333, 6.5300] at SS polar longitude 93.507844 on its graha-laghava day. Expected 94.0 (GL), 88 (Panchasiddhantika), 93 (SS), 93°3' (BSS) --> almost exact match with True_Revati!
+// Magha (Regulus) sidereal [126.6666, 0.3790] at SS polar longitude 126.7652541 on its graha-laghava day. Expected 129.00 (GL), 126.0 (Panchasiddhantika), 129 (SS), 129 (BSS)
+// Magha (Regulus) sidereal [129.0000, 0.3790] at SS polar longitude 129.1039601 on its graha-laghava day. Expected 129.00 (GL), 126.0 (Panchasiddhantika), 129 (SS), 129 (BSS) --> zero ayan day at [+0496/11/12 13.2207]
+// Citra (Spica) sidereal [180.0000, -1.9220] at SS polar longitude 179.1638906 on its graha-laghava day. Expected 183.00 (GL), 180°50' (Panchasiddhantika), 180 (SS), 183 (BSS)
+// Jyestha (Antares) sidereal [226.6666, -4.3512] at SS polar longitude 225.3336877 on its graha-laghava day. Expected 230.00 (GL), 229 (SS), 299°5' (BSS)
+// Jyestha (Antares) sidereal [230.0000, -4.3512] at SS polar longitude 228.7498217 on its graha-laghava day. Expected 230.00 (GL), 229 (SS), 299°5' (BSS) --> matches TRUE_REVATI
+// Mula (Sabik) sidereal [234.1200, 7.2000] at SS polar longitude 235.8856992 on its graha-laghava day. Expected 242.0 (GL), 241 (SS), 241 (BSS)
+// Mula (Shaula) sidereal [244.0000, -13.5100] at SS polar longitude 241.0867840 on its grha-laghava day. Expected 242.0 (GL), 241 (SS), 241 (BSS) --> close to TRUE_REVATI
+// Anuradha (Dscubba) sidereal [220.0000, -1.7300] at SS polar longitude 219.4185972 on its graha-laghava day. Expected 224.0 (GL), 224 (SS), 224°5' (BSS)
+// Ashlesha (,epHya) sidereal [109.0000, -11.2400] at SS polar longitude 107.5099665 on its graha-laghava day. Expected 107.0 (GL), 107.666 (Panchasiddhantika), 109 (SS), 108° (BSS)
+// Ashlesha (,alCnc) sidereal [109.0000, -5.2600] at SS polar longitude 108.2804385 on its graha-laghava day. Expected 107.0 (GL), 107.666 (Panchasiddhantika), 109 (SS), 108° (BSS) --> zero ayan day at [+0229/10/07 5.2840]
+// Pushya (deCnc) sidereal [106.0000, -0.0030] at SS polar longitude 105.9996404 on its graha-laghava day. Expected 106.0 (GL), 97.333 (Panchasiddhantika), 106 (SS), 106 (BSS)
+// Revati (zePsc) sidereal [359.6660, -0.2400] at SS polar longitude -0.2296270 on its graha-laghava day. Expected 0.0 (GL), 359°50' (SS), 0.0 (BSS)
